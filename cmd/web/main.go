3fe46b9a86f415e246eb4da30b3cf7dea806b97a
Calvin <nicxon.calvin@gmail.com>
2023-05-09 22:25:06 +0100

    add tests for main package

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -21,6 +21,24 @@ var session *scs.SessionManager
 
 // main is the main application function
 func main() {
+	err := run()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
+
+func run() error {
 	// what I am going to put in the session
 	gob.Register(models.Reservation{})
 
@@ -38,6 +56,7 @@ func main() {
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create tempalte cache")
+		return err
 	}
 
 	app.TemplateCache = tc
@@ -48,13 +67,5 @@ func main() {
 
 	render.NewTemplates(&app)
 
-	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
-
-	srv := &http.Server{
-		Addr:    portNumber,
-		Handler: routes(&app),
-	}
-
-	err = srv.ListenAndServe()
-	log.Fatal(err)
+	return nil
 }
